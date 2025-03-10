fe86feb420f52134286e616fdc73affa9438ec3a
JorgeCSH <jorge.cumminz@gmail.com>
2025-03-10 20:49:48 -0300

    Refactor: new dummy file

---
+++ tareas/dummy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// Dennis Ritchie version of Hello World
+void main() {
+    printf("hello, world");
+}
+
+// The other (and the one I have seen more)
+int main() {
+    printf("hello, world");
+    return 0;
+}
+
+
