f537f28d312d1ee69eee2a0781e312509e92d4f2
JorgeCSH <jorge.cumminz@gmail.com>
2025-03-22 19:32:08 -0300

    docs: update homework documentation

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -3,6 +3,26 @@
 
 #include "dup.h"
 
+/* 
+Homework 1, "Programacion de Software de Sistemas" CC3301
+Developer: Jorge Cummins
+Assignment due: saturday, march 31, 2025
+
+Preliminary information:
+I was told that the homework should be checked by the faculty members from the 
+section (assistants) just by running the assignment as specified in the instructions, 
+therefore, they might not be reading the main code. Considering this and knowing that
+there are no specific formal requirements for the code other than not using multiplication
+and division, I will be developing the assignment in english, from the documentation to the naming
+of variables. I took this desition because it might be a good instance for practicing my writing abilities 
+in english.
+
+Delopment information:
+Run native: linux, debian 12. 
+Run virtualbox: given image, windows 11 and debian 12.
+*/
+
+
 unsigned long long dupMasLargo(unsigned long long x) {
   // ... programe aca la funcion pedida ...
 }
