8c5361f2b6e5998a4ffc94937903c368a110fee2
JorgeCSH <jorge.cumminz@gmail.com>
2025-03-22 21:55:11 -0300

    feat: new bit counter created
    yes, like 2 hours just for a counter...

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -2,7 +2,39 @@
 #include <stdlib.h>
 
 #include "dup.h"
-
+/*
 unsigned long long dupMasLargo(unsigned long long x) {
   // ... programe aca la funcion pedida ...
+}
+*/
+
+// Funcion que cuenta la cantidad de bits
+int contador(unsigned long long x) {
+  // Definimos el contador
+  int n = 0;
+  /* Aca tengo unos prints porque debuggear es pa weones
+  printf("Valor original: x = %d", x);
+  printf(" y contador en estado inicial: n= %d", n);
+  */
+  while (x!=0) {
+    x = x>>1;
+    n++;
+    /* De nuevo, debuggear es para weones
+    printf("\nx= %d", x);
+    printf("\nn = %d", n);
+    printf("\n");
+    */
+  }
+  // printf("La cantidad de bits que hay es de: %d", n);
+  return n;
+}
+
+void main(){
+  // variable que consideraremos para las pruebas
+  unsigned long long x = 0b101010;
+
+  // Aca probamos el contador, que gran avance!
+  int poto = contador(x);
+  printf("La cantidad de bits que es: %d", poto);
+
 }
