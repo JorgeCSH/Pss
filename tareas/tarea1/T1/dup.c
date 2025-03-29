d2f97eee550a2a49ceb644042902a708e4e606d7
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-03-29 20:38:07 -0300

    refactor: change desing of the code

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -10,29 +10,18 @@ unsigned long long dupMasLargo(unsigned long long x) {
 	unsigned long long i = 0;
 	unsigned long long j = 0;
 
-	unsigned long long resultado = 0;	
-	
 	int k = 32;
-	if (x == 0) {
-		return (unsigned long long)1 << 37;
-	}
-	if ((x << 32) == ((x >> 32)<<32)) {
-		printf("\n oof");
-		return ((unsigned long long)1 << 37) | (x >> 32);
-	}
 	while (k) {
 		izq = (x << j)>> (32+i) ;
 		der = (x << (32 + i)) >> (32 + i);
 		if (izq	== der) {
-			unsigned long long largoSecuencia = (32-i);
-			
-			largoSecuencia = largoSecuencia << 32;
-			resultado = largoSecuencia | der;
-			break;
+			unsigned long long resultado = 0;	
+			resultado = ((32 - i) << 32) | der;
+			return resultado;
 		}
 		j += 2;
 		i++;
 		k--;
 	}
-	return resultado;
-
+	return 0;
+}
