bf24c8a0b86d9a658ff108d382a43281e2b88441
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-03-29 21:23:19 -0300

    feat: optimization

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -4,24 +4,17 @@
 #include "dup.h"
 
 unsigned long long dupMasLargo(unsigned long long x) {
-  unsigned long long izq = 0;
-  unsigned long long der = 0;
-  
-  unsigned long long i = 0;
-  unsigned long long j = 0;
-
-  int k = 32;
-  while (k) {
-    izq = (x << j)>> (32+i) ;
-    der = (x << (32 + i)) >> (32 + i);
-    if (izq== der) {
-      unsigned long long resultado = 0;
-      resultado = ((32 - i) << 32) | der;
-      return resultado;
+  unsigned long long izq, der;
+  unsigned long long i = 0, j = 0;
+    
+  for (unsigned long long k = 32; k > 0; k--) {
+    izq = (x << j) >> (32 + i);
+    der = x & (((unsigned long long)1 << (32 - i)) - 1);
+    if (izq == der) {
+      return ((unsigned long long)(32 - i) << 32) | der;
     }
     j += 2;
     i++;
-    k--;
   }
   return 0;
 }
