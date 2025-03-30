923c0578f72c6861fe72e6ac13afe12e7876feec
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-03-29 21:30:10 -0300

    feat: replace while with for and included comments. Replaced while for "for", this to optimize the time.

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -4,15 +4,26 @@
 #include "dup.h"
 
 unsigned long long dupMasLargo(unsigned long long x) {
-  unsigned long long izq, der;
-  unsigned long long i = 0, j = 0;
-    
+  // Variables que corresponden a los dos prefijos s.
+  unsigned long long izq;
+  unsigned long long der;
+
+  // Posiciones de los bits en los prefijos.
+  unsigned long long i = 0;
+  unsigned long long j = 0;
+ 
+  // Ciclo donde comparamos cada prefijo.  
   for (unsigned long long k = 32; k > 0; k--) {
+    // Subprefijo izquierdo, zSs.
     izq = (x << j) >> (32 + i);
+    // Subprefijo derecho, zsS.
     der = x & (((unsigned long long)1 << (32 - i)) - 1);
+    // Caso donde los subrefijos son iguales.
     if (izq == der) {
+      // cantidad de bits | subprefijo.
       return ((unsigned long long)(32 - i) << 32) | der;
     }
+    // Desplazamos los invariantes (o posiciones simplemente).
     j += 2;
     i++;
   }
