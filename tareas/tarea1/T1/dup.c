9a36c434bc9ad56cf58f132565c3047fcd2362fc
Jorge Cummins Holger <jorge.cumminz@gmail.com>
2025-03-30 23:32:01 -0300

    Update dup.c

--- tareas/tarea1/T1/dup.c
+++ tareas/tarea1/T1/dup.c
@@ -3,28 +3,7 @@
 
 #include "dup.h"
 
-/* 
-###############################################################################################################################
-###############################################################################################################################
-Homework 1, "Programacion de Software de Sistemas" CC3301
-Developer: Jorge Cummins
-Assignment due: saturday, march 31, 2025
-###############################################################################################################################
 
-Preliminary information:
-Preliminary Information: I was informed that the homework should be checked by the faculty members (assistants)
-in the section, simply by running the assignment as specified in the instructions.
-Therefore, they may not be reviewing the main code. Considering this, and knowing there are no specific formal
-requirements for the code other than avoiding multiplication and division, I will develop the assignment in English,
-from the documentation to the naming of variables. I made this decision because it provides a good opportunity to
-practice my writing skills in English.
-
-Development Information:
-- Native environment: Linux, Debian 12
-- VirtualBox setup: Installed on both Windows 11 and Debian 12.
-###############################################################################################################################
-###############################################################################################################################
-*/
 
 unsigned long long dupMasLargo(unsigned long long x) {
   // ... programe aca la funcion pedida ...
