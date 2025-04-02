e4ab9eab19527b4767629f28079d30a2d28b6d07
JorgeCSH <jorge.cumminz@gmail.com>
2025-04-02 11:40:11 -0300

    BREAKING CHANGE, feat: new tarea 2 directory For homework

---
+++ tareas/tarea2/T2/invertir.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "invertir.h"
+
+void invertir(char* s) {
+  /** Programe aqui la funcion invertir */
+}
+
+char *invertido(char *s) {
+  /** Programe aqui la funcion invertido */
+}
