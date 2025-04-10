2192fbb12d0b9aae7c5ae51f0ab7efe6828f6e90
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-04-10 17:32:56 -0400

    feat: update homework with solved mistake

--- tareas/tarea2/T2/invertir.c
+++ tareas/tarea2/T2/invertir.c
@@ -6,71 +6,58 @@
 
 void invertir(char* s) {
   /** Programe aqui la funcion invertir */
+
   /* Eliminamos espacios de mas. */
-  // Caracter de una copia del caracter actual en el que estamos posicionados del strign original.
-  char *caracterActual = s;
-  // Caracter donde iremos cambiando los espacios de mas por uno solo.
-  char *cambiarEspacio = s;
-  // Variable que usaremos como condicional mientras estemos posicionados sobre un espacio.
-  int noHayEspacio = 1;
-  // Ciclo mientras no hayamos recorrido todo el string.
-  while (*caracterActual != '\0') {
-    // Si encontramos un espacio, el caracter que representa los espacios se ira expandiendo en uno.
-    if (*caracterActual != ' ') {
-      // Expandimos el caracter del espacio.
-      *cambiarEspacio++ = *caracterActual;
-      // Cambiamos la situacion.
-      noHayEspacio = 0;
-      // Como detectamos un espacio, entonces hasta que no dejen de haber espacios reemplazamos la expresion que corresponde a el espacio total por uno solo.
-    } else if (!noHayEspacio) {
-      // Reemplazamos por uno solo.
-      *cambiarEspacio++ = ' ';
-      // Volvio a dejar dejar de haber espacios.
-      noHayEspacio = 1;
+  char *sConEspacios = s;
+  char *sSinEspacios = s;
+  int unEspacio = 1;
+  while (*sConEspacios != '\0') {
+    if (*sConEspacios != ' ') {
+      *sSinEspacios++ = *sConEspacios;
+      unEspacio = 0;
+    } else if (!unEspacio) {
+      *sSinEspacios++ = ' ';
+      unEspacio = 1;
     }
-    // Desplazamos por el caracter original.
-    caracterActual++;
+    sConEspacios++;
   }
-  // Corroboramos si nos pasamos del largo original. 
-  if (cambiarEspacio > s && *(cambiarEspacio - 1) == ' ') {
-    cambiarEspacio--;
+
+  char* auxUltimo = sSinEspacios;
+  auxUltimo--;
+  if (sSinEspacios > s && *auxUltimo == ' ') {
+    sSinEspacios--;
   }
-  // Como cambiar espacio apunta al caracter final despues de retirar los espacios extras, tenemos que agregar el \0 para indicar que ahora el string termina ahi.
-  *cambiarEspacio = '\0';
+  *sSinEspacios = '\0';
 
   /* Invertimos todo el string. */
-  // Esta parte esta inspirada en el aux 3.
-  // Inicio del string. 
   char *inicio = s;
-  // Final del string sin contar el \0 final.
   char *fin = s + strlen(s) - 1;
   while (inicio < fin) {
-    // Auxiliar que guarda el caracter antes de cambiarlo.
-    char auxDarVuelta = *inicio;
-    // Cambiamos el caracter por el del final. 
+    char auxInicio = *inicio;
     *inicio = *fin;
-    // El del final lo cambiamos por el guardado en el auxiliar.
-    *fin = auxDarVuelta;
-    // Nos desplazamos
+    *fin = auxInicio;
     inicio++;
     fin--;
   }
 
   /* Invertimos cada palabra por separado. */ 
-  char *palabra = s;
-  while (*palabra != '\0') {
-    while (*palabra == ' ') {
-      palabra++;  // Saltar espacios
+  char *sInvertir = s;
+  while (*sInvertir != '\0') {
+    // En caso de que nos posicionemos en un espacio, desplazamos al siguiente caracter.
+    if (*sInvertir == ' ') {
+      sInvertir++;
     }
-    char *inicioPalabra = palabra;
-    while (*palabra != '\0' && *palabra != ' ') {
-      palabra++;
+    // Buscamos hasta donde llega la palabra en la que estamos.
+    char *inicioPalabra = sInvertir;
+    while (*sInvertir != '\0' && *sInvertir != ' ') {
+      sInvertir++;
     }
-    char *finPalabra = palabra - 1;
+    // Damos vuelta la palabra.
+    char *finPalabra = sInvertir - 1;
     while (inicioPalabra < finPalabra) {
-      char auxPalabra = *inicioPalabra;
+      char auxInicioPalabra = *inicioPalabra;
       *inicioPalabra = *finPalabra;
-      *finPalabra = auxPalabra;
+      *finPalabra = auxInicioPalabra;
       inicioPalabra++;
       finPalabra--;
     }
@@ -79,72 +66,63 @@ void invertir(char* s) {
 
 char *invertido(char *s) {
   /** Programe aqui la funcion invertido */
-  /* Eliminamos los espacios extras, proceso analgo al anterior. */
-  char *auxOriginal = malloc(strlen(s) + 1);
 
-  char *caracterActual = s;
-  char *caracterSaltar = auxOriginal;
+  /* Alocamos espacios de memoria */
+  char *sResultado = malloc(strlen(s) + 1);
+  char *sOriginal = s;
 
-  int noHayEspacio = 1;
-  while (*caracterActual != '\0') {
-      if (*caracterActual != ' ') {
-          *caracterSaltar++ = *caracterActual;
-          noHayEspacio = 0;
-      } else if (!noHayEspacio) {
-          *caracterSaltar++ = ' ';
-          noHayEspacio = 1;
+  /* Eliminamos espacios de mas. */
+  char *sNuevo = sResultado;
+  int unEspacio = 1;
+  while (*sOriginal != '\0') {
+      if (*sOriginal != ' ') {
+          *sNuevo++ = *sOriginal;
+          unEspacio = 0;
+      } else if (!unEspacio) {
+          *sNuevo++ = ' ';
+          unEspacio = 1;
       }
-      caracterActual++;
+      sOriginal++;
   }
 
-  if (caracterSaltar > auxOriginal && *(caracterSaltar - 1) == ' ') {
-    caracterSaltar--;
+  char* auxUltimoNuevo = sNuevo;
+  auxUltimoNuevo--;
+  if (sNuevo > sResultado && *auxUltimoNuevo == ' ') {
+    sNuevo--;
   }
-  *caracterSaltar = '\0';
+  *sNuevo = '\0';
 
-  // Invertir todo el string
-  char *inicio = auxOriginal;
-  char *fin = auxOriginal + strlen(auxOriginal) - 1;
+  /* Invertimos todo el string. */
+  char *inicio = sResultado;
+  char *fin = sResultado + strlen(sResultado) - 1;
   while (inicio < fin) {
-      char tmp = *inicio;
+      char auxInicio = *inicio;
       *inicio = *fin;
-      *fin = tmp;
+      *fin = auxInicio;
       inicio++;
       fin--;
   }
 
-  // Invertir palabra por palabra
-  char *palabra = auxOriginal;
-  while (*palabra != '\0') {
-      while (*palabra == ' ') {
-      palabra++;
+  /* Invertimos palabra por palabra */
+  char *sNuevoInvertir = sResultado;
+  while (*sNuevoInvertir != '\0') {
+    if (*sNuevoInvertir == ' ') {
+      sNuevoInvertir++;
     }
-    char *inicioPalabra = palabra;
-    while (*palabra != '\0' && *palabra != ' ') {
-      palabra++;
+    char *inicioPalabra = sNuevoInvertir;
+    while (*sNuevoInvertir != '\0' && *sNuevoInvertir != ' ') {
+      sNuevoInvertir++;
     }
-    char *finPalabra = palabra - 1;
+    char *finPalabra = sNuevoInvertir - 1;
     while (inicioPalabra < finPalabra) {
-      char auxCaracter = *inicioPalabra;
+      char auxInicioPalabra = *inicioPalabra;
       *inicioPalabra = *finPalabra;
-      *finPalabra = auxCaracter;
+      *finPalabra = auxInicioPalabra;
       inicioPalabra++;
       finPalabra--;
     }
   }
 
-  // resultadoervar justo y copiar
-  char *resultado = malloc(strlen(auxOriginal) + 1);
-
-  char *caracterSinEspacio2 = auxOriginal;
-  char *caracterSaltar2 = resultado;
-  while (*caracterSinEspacio2) {
-      *caracterSaltar2 = *caracterSinEspacio2;
-      caracterSaltar2++;
-      caracterSinEspacio2++;
-  }
-  *caracterSaltar2 = '\0';  
-  free(auxOriginal);
-  return resultado;
+  return sResultado;
 }
 
