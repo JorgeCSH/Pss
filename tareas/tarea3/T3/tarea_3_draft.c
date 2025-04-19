32d36e7526f0a15463935e5938d0877e6eb2602b
Jorge Cummins <jorgecummins@Jorges-MacBook-Pro.local>
2025-04-19 15:26:51 -0400

    feat: structure and function to develop the homework

---
+++ tareas/tarea3/T3/tarea_3_draft.c
@@ -0,0 +1,20 @@
+#include <stdio.h>   // Para las funciones printf, fread, etc.
+
+typedef struct nodo {
+  int id;
+  struct nodo *izq, *der;
+}
+
+
+void elimIntervalo(Nodo **pa, int inf, int sup) {
+  // Complete aqui la funcion pedida en el enunciado
+}
+
+
+int main() {
+
+  //elimIntervalo();
+
+  printf("\n Este codigo ql funciono \n");
+  return 0;
+}
