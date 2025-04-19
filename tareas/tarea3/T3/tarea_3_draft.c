6af91568f8c8dd7ef9487196e39c66d23099b122
Jorge Cummins <jorgecummins@Jorges-MacBook-Pro.local>
2025-04-19 15:29:33 -0400

    feat: update draft

--- tareas/tarea3/T3/tarea_3_draft.c
+++ tareas/tarea3/T3/tarea_3_draft.c
@@ -6,6 +6,35 @@ typedef struct nodo {
 }
 
 
+Nodo *extractMax(Nodo **pa) {
+    if (*pa == NULL) {
+        return NULL;
+    }
+    if ((*pa)->der == NULL) {
+        
+        Nodo *res = (*pa);
+        *pa = (*pa)->izq;
+        return res;
+    } else {
+        return extractMax(&(*pa)->der);
+    }
+}
+
+Nodo *mezclar(Nodo **izq, Nodo **der) {
+    if (*izq == NULL) {
+        return *der;
+    }
+    if (*der == NULL) {
+        return *izq;
+    }
+    Nodo* res = extractMax(izq);
+    res->izq = *izq;
+    res->der = *der;
+    return res;
+}
+
+
+
 void elimIntervalo(Nodo **pa, int inf, int sup) {
   // Complete aqui la funcion pedida en el enunciado
 }
