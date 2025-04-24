b7761220aa8668ffade33d3019c122e4054b6490
JorgeCSH <jorge.cumminz@gmail.com>
2025-04-24 12:58:06 -0400

    feat: update matreaca

--- matraca/prec1.c
+++ matraca/prec1.c
@@ -5,31 +5,35 @@ void comprimir(char *s) {
   char *sAux1 = s; 
   char *sAux2 = s + 1;
   char *inicio = s;
-  int i =  0;
+  int i = 1;
 
   while(*sAux2 != '\0') {
     if (*sAux1 != *sAux2) {
       if(i > 1) {
-        *inicio = (char)i;
+        *inicio = i;
         inicio++;
-        i = 0;
-      } else {
-        i = 0;
-      }
+      }  
       *inicio = *sAux1;
       inicio++;
+      i = 0;
     }
+    i++;
     sAux1++;
     sAux2++;
   }
-  while (*inicio != '\0') {
-    *inicio = ' ';
-    inicio++;
+  if(*sAux2 == '\0') {
+    char *sAux3 = sAux1;
+    sAux3--;
+    if (*sAux3 != *sAux1) {
+      *inicio = *sAux1;
+      inicio++;
+    }
   }
+  *inicio = '\0';
 }
 
 int main() {
-  char *s = "AABCCCD";
+  char s[] = "AABCCCD";
   printf("s: %s\n  ", s);
   comprimir(s);
   printf("s: %s\n  ", s);
