340483ed9e5f918577ab4e7ea5ac90c9ba68da19
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-05-12 21:35:17 -0400

    refactor: last update to tarea

--- tareas/tarea4/T4/extraer.c
+++ tareas/tarea4/T4/extraer.c
@@ -3,98 +3,96 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  /* Verificamos la cantidad de valores entregados. */
+  // Verificamos la cantidad de valores ingresados.
   if (argc != 2) {
-    fprintf(stderr, "uso: %s archivo\n", argv[0]);
     exit(1);
   }
 
-  /* Cargar .*/
-  /* Cargamos el archivo. */
+  // cargamos el archivo
   FILE *f = fopen(argv[1], "r+");
 
-  /* Creamos valores. */
-  /* Aca creamos un buffer para la linea completa que se leera + 1, esto porque se tienen 16 caracteres mas el \n que no es considerado originalmente segun enunciado. */
+  // Variables.
   char buffer[16 + 1];
-  /* Creamos la variable que correspondera al texto de mayor prioridad. */
-  char textoPrioritario[11]; 
-  /* La prioridad corresponde a un valor de 6 caracteres, entonces la minima prioridad que puede alcanzar es de 999999. */
+  char textoPrioritario[10 + 1]; 
   int prioridadMayor = 999999;
-  /* */
   int posicionPersonaPrioritaria = 0;
 
-  /* Seccion de verificacion. */
-  /* Verificamos que el archivo "exista". */
+  // Verificamos que el archivo exista.
   if (f == NULL) {
     perror("error");
     exit(1);
   }
 
-  /* Verificamos los cuatro primeros caracteres de los elementos almacenados. Esto es, usamos fread para contar los caracteres que hay en la primera linea de la posicion 1 a la 4 y, de haber una cantidad diferente de 4 entonces el tamano no calza y arrojamos error. */ 
+  // Verificamos que, en la primera linea la cantidad de elementos tenga 4 caracteres.
   if (fread(buffer, 1, 4, f) != 4) {
-    fprintf(stderr, "error: no se pudo leer el tamano\n");
     exit(1);
   }
 
-  /*Convertimos la porcion inicial en un entero (esto segun la definicion que arrojo man atoi al ejecutarlo en la terminal. */
   int n = atoi(buffer);
 
-  /* Verificamos si la cola esta vacia o no identificando al valor que esta dentro de la cabecera.*/
+  // Verificamos si la cola esta vacia.
   if (n == 0) {
     fprintf(stderr, "error: %s esta vacia\n", argv[1]);
     exit(1);
   }
 
-  /* Extraemos el texto. */
+  /* Recorremos el archivo leyendo las lineas. */
+  long posicion;
+  char lineaLeida[(16 + 1) + 1];
+  char textoLeido[10 + 1];
+  char prioridadLeida[6 + 1];
   for (int i = 0; i < n; i++) {
-    long pos = 4 + 1 + i * 17;
-    if (fseek(f, pos, SEEK_SET) != 0) {
+    posicion = 4 + 1 + i * (16 + 1);
+    // Verificamos que el texto tenga caracteres.
+    if (fseek(f, posicion, SEEK_SET) != 0) {
       exit(1);
     }
-    char linea[18];
-    if (fread(linea, 1, 17, f) != 17) {
+    // Verificamos que la linea leida no tenga mas de 17 caracteres.
+    if (fread(lineaLeida, 1, (16+1), f) <  17) {
       exit(1);
     }
 
-    linea[17] = '\0';
-    char texto[11];
-    strncpy(texto, linea, 10);
-    texto[10] = '\0';
-
-    char prioridadLeida[7];
-    strncpy(prioridadLeida, linea + 10, 6);
+    lineaLeida[16+1] = '\0';
+    memcpy(textoLeido, lineaLeida, 10);
+    textoLeido[10] = '\0';
+    memcpy(prioridadLeida, lineaLeida + 10, 6);
     prioridadLeida[6] = '\0';
+    
     int prioridad = atoi(prioridadLeida);
     if (prioridad < prioridadMayor) {
       prioridadMayor = prioridad;
       posicionPersonaPrioritaria = i;
-      strncpy(textoPrioritario, texto, 11);
+      strncpy(textoPrioritario, textoLeido, 11);
     }
   }
-
-  // Mostramos el texto solicitado 
+  // Mostramos el texto con mayor prioridad.
   printf("%s\n", textoPrioritario);
 
-  long ultima_pos = 4 + 1 + (n - 1) * 17;
+  /* Mover elementos. */
+  // Caso donde el elemento no corresponde al ultimo.
+  long personaFinal = 4 + 1 + (n - 1) * (16+1);
+  long personaPrioritaria = 4 + 1 + posicionPersonaPrioritaria * (16+1);
+  char bufferMover[16+1];
   if (posicionPersonaPrioritaria != n - 1) {
-    if (fseek(f, ultima_pos, SEEK_SET) != 0) {
+    // Verificamos que el espacio esta vacio.
+    if (fseek(f, personaFinal, SEEK_SET) != 0) {
       exit(1);
     }
-    char ultimo[17];
-    if (fread(ultimo, 1, 17, f) != 17) {
+    // Verificamos que el espacio es suficiente
+    if (fread(bufferMover, 1, (16+1), f) < 17) {
       exit(1);
     }
-
-    long mejor_pos = 4 + 1 + posicionPersonaPrioritaria * 17;
-    if (fseek(f, mejor_pos, SEEK_SET) != 0) {
+    if (fseek(f, personaPrioritaria, SEEK_SET) != 0) {
       exit(1);
     }
-    fwrite(ultimo, 1, 17, f);
+    // Reescribimos.
+    fwrite(bufferMover, 1, (16+1), f);
   }
 
-   if (fseek(f, 0, SEEK_SET) != 0) {
-     exit(1);
-   }
+  // Caso donde el elemento corresponde al ultimo.
+  if (fseek(f, 0, SEEK_SET) != 0) {
+    exit(1);
+  }
   sprintf(buffer, "%-4d", n - 1);
   fwrite(buffer, 1, 4, f);
 
