50f839f0a275f6b52904a2931b089f762c37e60f
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-05-12 20:00:43 -0400

    docs: tarea 4 documentation

--- tareas/tarea4/T4/extraer.c
+++ tareas/tarea4/T4/extraer.c
@@ -3,33 +3,48 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+  /* Verificamos la cantidad de valores entregados. */
   if (argc != 2) {
     fprintf(stderr, "uso: %s archivo\n", argv[0]);
     exit(1);
   }
 
+  /* Cargar .*/
+  /* Cargamos el archivo. */
   FILE *f = fopen(argv[1], "r+");
+
+  /* Creamos valores. */
+  /* Aca creamos un buffer para la linea completa que se leera + 1, esto porque se tienen 16 caracteres mas el \n que no es considerado originalmente segun enunciado. */
+  char buffer[16 + 1];
+  /* Creamos la variable que correspondera al texto de mayor prioridad. */
+  char mejor_texto[11]; 
+  /* La prioridad corresponde a un valor de 6 caracteres, entonces la minima prioridad que puede alcanzar es de 999999. */
+  int mejor_prioridad = 999999;
+  /* */
+  int mejor_indice = 0;
+
+  /* Seccion de verificacion. */
+  /* Verificamos que el archivo "exista". */
   if (f == NULL) {
     perror("error");
     exit(1);
   }
 
-  char buffer[17];
+  /* Verificamos los cuatro primeros caracteres de los elementos almacenados. Esto es, usamos fread para contar los caracteres que hay en la primera linea de la posicion 1 a la 4 y, de haber una cantidad diferente de 4 entonces el tamano no calza y arrojamos error. */ 
   if (fread(buffer, 1, 4, f) != 4) {
     fprintf(stderr, "error: no se pudo leer el tamaño\n");
     exit(1);
   }
-  buffer[5] = '\0';
+
+  /*Convertimos la porcion inicial en un entero (esto segun la definicion que arrojo man atoi al ejecutarlo en la terminal. */
   int n = atoi(buffer);
 
+  /* Verificamos si la cola esta vacia o no identificando al valor que esta dentro de la cabecera.*/
   if (n == 0) {
     fprintf(stderr, "error: %s esta vacia\n", argv[1]);
     exit(1);
   }
 
-  char mejor_texto[11]; 
-  int mejor_prioridad = 999999;
-  int mejor_indice = -1;
 
   for (int i = 0; i < n; i++) {
     long pos = 4 + 1 + i * 17;
@@ -60,6 +75,7 @@ int main(int argc, char *argv[]) {
       }
   }
 
+  // Mostramos el texto solicitado 
   printf("%s\n", mejor_texto);
 
   long ultima_pos = 4 + 1 + (n - 1) * 17;
@@ -83,12 +99,11 @@ int main(int argc, char *argv[]) {
    fwrite(ultimo, 1, 17, f);
   }
 
-  n--;
   if (fseek(f, 0, SEEK_SET) != 0) {
     fprintf(stderr, "error: fseek al inicio fallo\n");
     exit(1);
   }
-  sprintf(buffer, "%-4d", n);
+  sprintf(buffer, "%-4d", n - 1);
   fwrite(buffer, 1, 4, f);
 
   fclose(f);
