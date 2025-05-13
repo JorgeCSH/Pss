315dd5d7f8658df1bb9be997c4b06d991fb684e0
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-05-12 20:31:51 -0400

    refactor: tarea 4

--- tareas/tarea4/T4/extraer.c
+++ tareas/tarea4/T4/extraer.c
@@ -17,11 +17,11 @@ int main(int argc, char *argv[]) {
   /* Aca creamos un buffer para la linea completa que se leera + 1, esto porque se tienen 16 caracteres mas el \n que no es considerado originalmente segun enunciado. */
   char buffer[16 + 1];
   /* Creamos la variable que correspondera al texto de mayor prioridad. */
-  char mejor_texto[11]; 
+  char textoPrioritario[11]; 
   /* La prioridad corresponde a un valor de 6 caracteres, entonces la minima prioridad que puede alcanzar es de 999999. */
-  int mejor_prioridad = 999999;
+  int prioridadMayor = 999999;
   /* */
-  int mejor_indice = 0;
+  int posicionPersonaPrioritaria = 0;
 
   /* Seccion de verificacion. */
   /* Verificamos que el archivo "exista". */
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
 
   /* Verificamos los cuatro primeros caracteres de los elementos almacenados. Esto es, usamos fread para contar los caracteres que hay en la primera linea de la posicion 1 a la 4 y, de haber una cantidad diferente de 4 entonces el tamano no calza y arrojamos error. */ 
   if (fread(buffer, 1, 4, f) != 4) {
-    fprintf(stderr, "error: no se pudo leer el tamaño\n");
+    fprintf(stderr, "error: no se pudo leer el tamano\n");
     exit(1);
   }
 
@@ -45,64 +45,56 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-
+  /* Extraemos el texto. */
   for (int i = 0; i < n; i++) {
     long pos = 4 + 1 + i * 17;
-      if (fseek(f, pos, SEEK_SET) != 0) {
-        fprintf(stderr, "error: fseek fallo\n");
-        exit(1);
-      }
-      char linea[18];
-      if (fread(linea, 1, 17, f) != 17) {
-        fprintf(stderr, "error: lectura incompleta\n");
-        exit(1);
-      }
-      linea[17] = '\0';
-
-      char texto[11];
-      strncpy(texto, linea, 10);
-      texto[10] = '\0';
-
-      char prioridad_str[7];
-      strncpy(prioridad_str, linea + 10, 6);
-      prioridad_str[6] = '\0';
-      int prioridad = atoi(prioridad_str);
-
-      if (prioridad < mejor_prioridad) {
-        mejor_prioridad = prioridad;
-        mejor_indice = i;
-        strncpy(mejor_texto, texto, 11);
-      }
-  }
-
-  // Mostramos el texto solicitado 
-  printf("%s\n", mejor_texto);
-
-  long ultima_pos = 4 + 1 + (n - 1) * 17;
-  if (mejor_indice != n - 1) {
-    if (fseek(f, ultima_pos, SEEK_SET) != 0) {
-      fprintf(stderr, "error: fseek fallo al leer ultimo\n");
+    if (fseek(f, pos, SEEK_SET) != 0) {
+      exit(1);
+    }
+    char linea[18];
+    if (fread(linea, 1, 17, f) != 17) {
       exit(1);
     }
 
-   char ultimo[17];
-   if (fread(ultimo, 1, 17, f) != 17) {
-     fprintf(stderr, "error: no se pudo leer el ultimo\n");
-     exit(1);
-   }
+    linea[17] = '\0';
+    char texto[11];
+    strncpy(texto, linea, 10);
+    texto[10] = '\0';
 
-   long mejor_pos = 4 + 1 + mejor_indice * 17;
-   if (fseek(f, mejor_pos, SEEK_SET) != 0) {
-     fprintf(stderr, "error: fseek fallo al escribir\n");
-     exit(1);
-   }
-   fwrite(ultimo, 1, 17, f);
+    char prioridadLeida[7];
+    strncpy(prioridadLeida, linea + 10, 6);
+    prioridadLeida[6] = '\0';
+    int prioridad = atoi(prioridadLeida);
+    if (prioridad < prioridadMayor) {
+      prioridadMayor = prioridad;
+      posicionPersonaPrioritaria = i;
+      strncpy(textoPrioritario, texto, 11);
+    }
   }
 
-  if (fseek(f, 0, SEEK_SET) != 0) {
-    fprintf(stderr, "error: fseek al inicio fallo\n");
-    exit(1);
+  // Mostramos el texto solicitado 
+  printf("%s\n", textoPrioritario);
+
+  long ultima_pos = 4 + 1 + (n - 1) * 17;
+  if (posicionPersonaPrioritaria != n - 1) {
+    if (fseek(f, ultima_pos, SEEK_SET) != 0) {
+      exit(1);
+    }
+    char ultimo[17];
+    if (fread(ultimo, 1, 17, f) != 17) {
+      exit(1);
+    }
+
+    long mejor_pos = 4 + 1 + posicionPersonaPrioritaria * 17;
+    if (fseek(f, mejor_pos, SEEK_SET) != 0) {
+      exit(1);
+    }
+    fwrite(ultimo, 1, 17, f);
   }
+
+   if (fseek(f, 0, SEEK_SET) != 0) {
+     exit(1);
+   }
   sprintf(buffer, "%-4d", n - 1);
   fwrite(buffer, 1, 4, f);
 
