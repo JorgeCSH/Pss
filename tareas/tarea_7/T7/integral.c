e2aba86205c6afd9b9b104205f1e4ee8ba31964d
JorgeCSH <jorge.cummins@ug.uchile.cl>
2025-06-22 00:45:33 -0400

    feat: working non-documented homework

--- tareas/tarea_7/T7/integral.c
+++ tareas/tarea_7/T7/integral.c
@@ -9,6 +9,7 @@
 
 double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
   int fdi[p][2]; 
+  pid_t pid[p];
   double h = (xf - xi) / p;
   int trapecios = n / p;
   double integral_numerica = 0;
@@ -20,28 +21,26 @@ double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
     }
   }
 
-  pid_t pid = fork();
   for (int i = 0; i < p; i++) {
-    if (pid == 0) {
+    pid[i] = fork();
+    if (pid[i] == 0) {
       close(fdi[i][0]);
-      double local_xi = xi + i * h;
-      double local_xf = local_xi + h;
-      double integ_paso = integral(f, ptr, local_xi, local_xf, trapecios);
+      double paso = xi + i * h;
+      double integ_paso = integral(f, ptr, paso, paso + h, trapecios);
       write(fdi[i][1], &integ_paso, sizeof(double));
       close(fdi[i][1]);
+      exit(0);
     } else {
       close(fdi[i][1]);
-      double integral_paso;
-      read(fdi[i][0], &integral_paso, sizeof(double));
-      integral_numerica += integral_paso; 
-      close(fdi[i][0]);
     }
   }
 
-  if (pid == 0) {
-    exit(0);
-  } else {
-    waitpid(pid, NULL, 0);
+  for (int i = 0; i < p; i++) {
+    double integ_paso;
+    read(fdi[i][0], &integ_paso, sizeof(double));
+    close(fdi[i][0]);
+    integral_numerica += integ_paso;
+    waitpid(fdi[i][2], NULL, 0); 
   }
 
   return integral_numerica;
