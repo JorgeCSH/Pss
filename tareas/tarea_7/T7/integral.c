811cdd35f9ceebf3227a84087fe28d14e264d6d5
Jorge Cummins Holger <jorge.cumminz@gmail.com>
2025-06-22 19:31:53 -0400

    refactor: integra.c

--- tareas/tarea_7/T7/integral.c
+++ tareas/tarea_7/T7/integral.c
@@ -8,7 +8,7 @@
 #include "integral.h"
 
 double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
-  int fdi[p][2]; 
+  int fds[p][2]; 
   pid_t pid[p];
   double h = (xf - xi) / p;
   int trapecios = n / p;
@@ -16,7 +16,7 @@ double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
 
   /* Crear p pipes, uno por cada hijo. */
   for (int i = 0; i < p; i++) {
-    if (pipe(fdi[i]) == -1) {
+    if (pipe(fd[i]) == -1) {
       perror("pipe");
       exit(1);
     }
@@ -26,22 +26,22 @@ double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
   for (int i = 0; i < p; i++) {
     pid[i] = fork();
     if (pid[i] == 0) {
-      close(fdi[i][0]);
+      close(fds[i][0]);
       double paso = xi + i * h;
       double integ_paso = integral(f, ptr, paso, paso + h, trapecios);
-      write(fdi[i][1], &integ_paso, sizeof(double));
-      close(fdi[i][1]);
+      write(fds[i][1], &integ_paso, sizeof(double));
+      close(fds[i][1]);
       exit(0);
     } else {
-      close(fdi[i][1]);
+      close(fds[i][1]);
     }
   }
 
   /* Leer resultados padre y esperar terminar hijos. */
   for (int i = 0; i < p; i++) {
     double integ_paso;
-    read(fdi[i][0], &integ_paso, sizeof(double));
-    close(fdi[i][0]);
+    read(fds[i][0], &integ_paso, sizeof(double));
+    close(fds[i][0]);
     integral_numerica += integ_paso;
     waitpid(pid[i], NULL, 0); 
   }
